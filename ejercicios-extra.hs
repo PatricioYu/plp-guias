a1e9f7bb898ac5eef0fa0e6d379e63170737ea6b
patricio yu <yupatricio0@gmail.com>
2025-08-25 20:08:54 -0300

    se agregan ejercicios extra planteados en clase

---
+++ ejercicios-extra.hs
@@ -0,0 +1,16 @@
+-- implementacion del mergeSort
+
+merge :: (Ord a) => [a] -> [a] -> [a]
+merge [] [] = []
+merge [] xs = xs
+merge xs [] = xs
+merge (x:xs) (y:ys) 
+    | x <= y = x:merge xs (y:ys)
+    | otherwise = y:merge ys (x:xs)
+
+mergeSort :: (Ord a) => [a] -> [a]
+mergeSort [] = []
+mergeSort [x] = [x]
+mergeSort xs = 
+    let (left, right) = splitAt (length xs `div` 2) xs
+    in merge (mergeSort left) (mergeSort right)
