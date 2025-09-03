73aac50743cb921d03f1068198aa3179023dc4e5
patricio yu <yupatricio0@gmail.com>
2025-09-02 22:11:14 -0300

    ej3 completado

--- guia-1.hs
+++ guia-1.hs
@@ -30,7 +30,7 @@ uncurryIndio :: (a -> b -> c) -> (a, b) -> c
 uncurryIndio f (a, b) = f a b
 
 -------------------- ESQUEMAS DE RECURSIÓN --------------------
--- 3) Redifinir usando foldr las funciones sum, elem, (++), filter y map
+-- 3)a) Redifinir usando foldr las funciones sum, elem, (++), filter y map
 sumaFoldr :: [Int] -> Int
 sumaFoldr = foldr (+) 0
 
@@ -52,7 +52,48 @@ concatFoldr = flip (foldr (:))
 -- [1,2,3,4,5,6]
 
 filterFoldr :: (a -> Bool) -> [a] -> [a]
-filterFoldr p = foldr (\x rec -> if p x == True then x:rec else rec) []
+filterFoldr p = foldr (\x rec -> if p x then x:rec else rec) []
 
 mapFoldr :: (a -> b) -> [a] -> [b]
-mapFoldr f = foldr (\x rec -> (f x):rec) []
+-- mapFoldr f = foldr (\x rec -> f x:rec) []
+mapFoldr f = foldr ((:) . f) []
+
+-- 3)b) devuelve el maximo elemento de la lista segun una funcion de comparacion utilizando foldr. xej maximum = mejorSegun (>)
+mejorSegun :: (a -> a -> Bool) -> [a] -> a
+mejorSegun f = foldr1 (\x rec -> if f x rec then x else rec)
+
+-- 3)c) dada una lista de numeros devuelve otra de la misma longitud, que tiene en cada posicion la suma parcial de los elementos de la lista original desde la cabeza hasta la posicion actual. xej, sumasParciales [1,4,-1,0,5] ~> [1,5,4,4,9]
+
+sumasParciales :: Num a => [a] -> [a]
+sumasParciales = foldl (\ac a -> if null ac then [a] else ac ++ [a + last ac]) []
+
+-- 3)d)
+sumaAlt :: Num a => [a] -> a
+sumaAlt = foldr (-) 0
+-- foldr [1,2,3,4,5]
+-- 1 - (foldr [2,3,4,5])
+-- 1 - (2 - foldr [3,4,5])
+-- 1 - (2 - (3 - foldr[4,5]))
+-- 1 - (2 - (3 - (4 - foldr [5])))
+-- 1 - (2 - (3 - (4 - 5))))
+-- 1 - (2 - (3 - (-1)))
+-- 1 - (2 - 4)
+-- 1 - (-2)
+-- 3
+
+-- 3)e)
+sumaAltReves :: Num a => [a] -> a
+sumaAltReves = foldr (-) 0 . reverse
+-- sumaAltReves = foldl (\ac x -> x - ac) 0
+
+-- 4)a) dada una lista devuelve todas sus permutaciones
+permutaciones :: [a] -> [[a]]
+permutaciones = foldr (\x rec -> concatMap (\xs -> map (f x xs) [0..length xs]) rec) [[]]
+    where f x xs index = drop index xs ++ [x] ++ take index xs
+
+-- [3,1,2]
+-- 3 * 1 * (map f 2 [] [0..0]) --> 3 * 1 * (f 2 [] 0 = drop 0 [] ++ [2] ++ take 0 []) = [2]
+-- 3 * (map f 1 [2] [0..1]) --> [drop 0 [2] ++ [1] ++ take 0 [2]] ++ [drop 1 [2] ++ [1] ++ take 1 [2]] = [[1,2]]
+
+-- >>> permutaciones [3,1,2]
+-- [[2,1,3],[1,3,2],[3,2,1],[1,2,3],[2,3,1],[3,1,2]]
